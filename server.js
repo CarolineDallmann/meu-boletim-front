15d21619850f38daa8c7f79896296a2bca1037ff
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-05-09 20:30:16 -0300

    dir dist

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const port = process.env.PORT || 8081;
 const app = express();
-app.use(favicon(__dirname + '/dist/favicon.ico'));
+app.use(favicon(__dirname + '/dist/meu-boletim-front/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/meu-boletim-front')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/meu-boletim-front', 'index.html'));
 });
 app.listen(port);
