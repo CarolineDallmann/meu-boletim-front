525d1d66071d5b9b27aa8ccfb26f8f0fff29ba0a
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-05-27 08:54:45 -0300

    criação e aplicação pipe série

---
+++ src/app/pipes/serie-de-para.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SerieDeParaPipe } from './serie-de-para.pipe';
+
+describe('SerieDeParaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SerieDeParaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
