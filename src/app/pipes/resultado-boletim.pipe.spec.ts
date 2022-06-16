fdf92aa0d944159651f0650bc6fb430c01e2e2de
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-06-16 19:13:49 -0300

    testes e specs

--- src/app/pipes/resultado-boletim.pipe.spec.ts
+++ src/app/pipes/resultado-boletim.pipe.spec.ts
@@ -1,8 +1,23 @@
+import { ResultadoBoletim } from '../enums/resultado-boletim.enum';
 import { ResultadoBoletimPipe } from './resultado-boletim.pipe';
 
 describe('ResultadoBoletimPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ResultadoBoletimPipe();
-    expect(pipe).toBeTruthy();
+  it('traducoes', () => {
+    expect(
+      new ResultadoBoletimPipe().transform(ResultadoBoletim.APROVADO)
+    ).toBe('Aprovado');
+    expect(
+      new ResultadoBoletimPipe().transform(ResultadoBoletim.REPROVADO)
+    ).toBe('Reprovado');
+    expect(
+      new ResultadoBoletimPipe().transform(
+        ResultadoBoletim.REPROVADO_POR_FALTAS
+      )
+    ).toBe('Reprovado por faltas');
+  });
+  it('vazio', () => {
+    expect(
+      new ResultadoBoletimPipe().transform(ResultadoBoletim.INDETERMINADO)
+    ).toBe('');
   });
 });
